8e5565fce4a6039b51d4fed0321e6133cf359b32
JorgeIvanGF_WSL <jorgeivangf@gmail.com>
2025-06-27 22:14:32 +0200

    CHORE(exe01): Brain header and cpp files created. Addition of the Bran* to Dog Class.

--- ex01/inc/Brain.hpp
+++ ex01/inc/Brain.hpp
@@ -1,35 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jorgutie <jorgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/27 21:53:44 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/06/27 22:10:43 by jorgutie         ###   ########.fr       */
+/*   Created: 2025/06/27 21:57:00 by jorgutie          #+#    #+#             */
+/*   Updated: 2025/06/27 22:11:16 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
 
-#ifndef DOG_HPP
-#define DOG_HPP
+#include <iostream>
 
-#include "Animal.hpp"
-#include "Brain.hpp" // New
-
-class Dog : public Animal
+class Brain
 {
 	private:
-		Brain* _brain; //Brain Pointer
-	
-	public:
-		Dog();                            // Default Constructor
-		Dog(const Dog& other);            // Copy Constructor
-		Dog& operator=(const Dog& other); // Assignment operator
-		~Dog();
-		
-		void makeSound() const;
-};
+		std::string _ideas[100];
 
+	public:
+		Brain();                            // Default constructor
+		Brain(const Brain& other);         // Copy constructor
+		Brain& operator=(const Brain& other); // Assignment operator
+		~Brain();                           // Destructor
+
+		std::string getIdea(int index) const;
+		void setIdea(int index, const std::string& idea);
+};
 
 #endif
